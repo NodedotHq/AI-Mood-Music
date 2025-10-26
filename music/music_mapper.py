76dfbb5caa3cd8da89dad47f907faad1ce475105
sohmxdd <soham.mishra206@gmail.com>
2025-10-26 17:52:58 +0530

    Fixed emotion-to-song mapping in music_mapper.py

--- music/music_mapper.py
+++ music/music_mapper.py
@@ -1,12 +1,12 @@
 # music/music_mapper.py
 EMOTION_TO_SONG = {
-    "happy": "assets/songs/Good Life.mp3",
-    "sad": "assets/songs/Drake - Marvin's Room (Official Video).mp3",
-    "angry": "assets/songs/Kanye West (Ye) - Heil Hitler (MUSIC VIDEO).mp3",
-    "neutral": "assets/songs/J. Cole - Love Yourz (Lyrics).mp3",
-    "surprise": "assets/songs/Travis Scott - SICKO MODE (Lyrics) ft. Drake.mp3",
-    "fear": "assets/songs/Kendrick Lamar - Not Like Us (Lyrics) Drake Diss.mp3",
-    "disgust": "assets/songs/Kendrick Lamar - Not Like Us (Lyrics) Drake Diss.mp3"
+    "happy": "Good Life.mp3",
+    "sad": "Drake - Marvin's Room (Official Video).mp3",
+    "angry": "Kanye West (Ye) - Heil Hitler (MUSIC VIDEO).mp3",
+    "neutral": "J. Cole - Love Yourz (Lyrics).mp3",
+    "surprise": "Travis Scott - SICKO MODE (Lyrics) ft. Drake.mp3",
+    "fear": "Kendrick Lamar - Not Like Us (Lyrics) Drake Diss.mp3",
+    "disgust": "Kendrick Lamar - Not Like Us (Lyrics) Drake Diss.mp3"
 }
 
 def get_song_for_emotion(emotion: str) -> str:
