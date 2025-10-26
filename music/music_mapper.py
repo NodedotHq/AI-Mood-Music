81dbc086427e4b29d16a3e053d2fae072c824039
node <nodedot.in>
2025-10-26 17:17:59 +0530

    songs

--- music/music_mapper.py
+++ music/music_mapper.py
@@ -1,19 +1,16 @@
+# music/music_mapper.py
 EMOTION_TO_SONG = {
-    "happy": "assets/songs/happy.mp3",
-    "sad": "assets/songs/sad.mp3",
-    "angry": "assets/songs/angry.mp3",
-    "neutral": "assets/songs/chill.mp3",
-    "surprise": "assets/songs/surprise.mp3",
-    "fear": "assets/songs/mysterious.mp3",
-    "disgust": "assets/songs/weird.mp3"
+    "happy": "assets/songs/Good Life.mp3",
+    "sad": "assets/songs/Drake - Marvin's Room (Official Video).mp3",
+    "angry": "assets/songs/Kanye West (Ye) - Heil Hitler (MUSIC VIDEO).mp3",
+    "neutral": "assets/songs/J. Cole - Love Yourz (Lyrics).mp3",
+    "surprise": "assets/songs/Travis Scott - SICKO MODE (Lyrics) ft. Drake.mp3",
+    "fear": "assets/songs/Kendrick Lamar - Not Like Us (Lyrics) Drake Diss.mp3",
+    "disgust": "assets/songs/Kendrick Lamar - Not Like Us (Lyrics) Drake Diss.mp3"
 }
 
 def get_song_for_emotion(emotion: str) -> str:
-    """
-    Returns the song file path corresponding to a detected emotion.
-    """
     return EMOTION_TO_SONG.get(emotion.lower(), "assets/songs/default.mp3")
 
-# Quick test
 if __name__ == "__main__":
-    print(get_song_for_emotion("happy"))  # should print assets/songs/happy.mp3
+    print(get_song_for_emotion("happy"))
